9d9590618fe30ef6d11d4e7bd1e2d010033c5fe4
hongtae <ghdxo4588@naver.com>
2025-11-07 23:33:00 +0900

    feat(서버 오픈) : 서버 오픈하기 오류 수정

--- Frontend/client/vite.config.js
+++ Frontend/client/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+// Frontend/client/vite.config.js
+import { defineConfig } from 'vite';
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 5173,
+    open: '/support-matching-prototype.html'
+  }
+});
